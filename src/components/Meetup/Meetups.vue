4abc9fb60f091b1e67ca045dc13e7ba931cc68ca
lee <kwlsy666@naver.com>
2018-02-01 17:54:55 +0900

    meetup 8강 진행중

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <p>Meetups Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5>
+                <v-card-media
+                src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
+                height="130px">
+                </v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div>
+                    <h5 class="white--text mb-0">My Meetup</h5>
+                    <div>17th July 2017</div>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat>
+                    <v-icon left light>arrow_forward</v-icon>
+                    View Meetup
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+
 </template>
