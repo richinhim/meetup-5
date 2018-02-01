a5eedfe915577b41bf416380f2de243a6a6a4827
lee <kwlsy666@naver.com>
2018-02-01 17:30:07 +0900

    meetup 7강 완료

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,14 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav"
       class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title"
-          router
+        <v-btn flat v-for="item in menuItems" :key="item.title"          
           :to="item.link"
         >
           <v-icon left dark>{{item.icon}}</v-icon>
