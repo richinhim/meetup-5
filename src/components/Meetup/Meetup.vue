0d67c822dbf2664c74f524ba1a23aee58018609f
lee <kwlsy666@naver.com>
2018-02-02 16:16:00 +0900

    meetup 9강 완료 Manag State with Vuex

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -4,14 +4,14 @@
       <v-flex xs12>
         <v-card>
           <v-card-title>
-            <h4 class="primary--text">My Meetup</h4>
+            <h4 class="primary--text">{{ meetup.title}}</h4>
           </v-card-title>
           <v-card-media
-          src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
+          :src="meetup.imageUrl"
           height="400px">
           </v-card-media>
           <v-card-text>
-            <div class="info--text">17th July 2017 - Where it takes place
+            <div class="info--text">{{ meetup.date }} - Where it takes place
             </div>
             <div>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -28,3 +28,13 @@
     </v-layout>
   </v-container>
 </template>
+<script>
+  export default {
+    props: ['id'],
+    computed: {
+      meetup () {
+        return this.$store.getters.loadedMeetup(this.id)
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import { store } from './store'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, { theme: {
@@ -22,6 +23,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
